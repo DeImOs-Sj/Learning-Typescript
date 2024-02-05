9d399dfa824f5653c021ab359143675632527fd7
Your Name <shlokjagtap123@gmail.com>
2024-02-05 20:29:23 +0530

    functions done

---
+++ functions.js
@@ -0,0 +1,3 @@
+var array = ['hello', 'bye'];
+var array2 = array.map(function (cb) { return cb + '1'; });
+console.log(array2);
---
+++ functions.ts
@@ -0,0 +1,5 @@
+const array = ['hello', 'bye'];
+
+const array2 = array.map(cb => cb + '1');
+console.log(array2);
+export { }
