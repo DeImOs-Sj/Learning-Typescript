b359b615e790fc6b47da4df2f42b15ac4372470c
Your Name <shlokjagtap123@gmail.com>
2024-02-06 13:50:24 +0530

    type defining

---
+++ objects.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
+    name: "shlok",
+    roll_no: 2,
+    email: "shlok2@gmail.com"
+};
+function createUser() {
+    return User.email;
+}
+var user = createUser();
+console.log(user);
---
+++ objects.ts
@@ -0,0 +1,13 @@
+const User = {
+    name: "shlok",
+    roll_no: 2,
+    email: "shlok2@gmail.com"
+
+}
+
+function createUser() {
+    return User.email
+}
+const user = createUser()
+console.log(user)
+export { }
